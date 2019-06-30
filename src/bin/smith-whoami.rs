75329f73200e3bf119f75b7bcd6a554b90dbf44c
Mark Hibberd <mark@hibberd.id.au>
2019-06-30 20:02:18 +1000

    Really basic oauth2 flow.

---
+++ src/bin/smith-whoami.rs
@@ -0,0 +1,4 @@
+extern crate smith;
+
+fn main() {
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
-pub mod version;
+extern crate biscuit;
+extern crate dirs;
+extern crate reqwest;
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+extern crate yasna;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub mod api;
+pub mod codec;
+pub mod configuration;
+pub mod version;
